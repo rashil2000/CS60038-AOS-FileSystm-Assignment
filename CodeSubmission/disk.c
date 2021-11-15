f03d202e1cd51975ebcb5e5e23b9c3d48047fc68
Rashil Gandhi <rashil2000@gmail.com>
2021-11-15 11:35:23 +0530

    Part A done

--- CodeSubmission/disk.c
+++ CodeSubmission/disk.c
@@ -1,1 +1,56 @@
 #include "disk.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+disk *create_disk(int nbytes)
+{
+	int nblocks = (nbytes - 24) / BLOCKSIZE;
+
+	disk* ret = malloc(sizeof(disk));
+	ret->size = nbytes;
+	ret->blocks = nblocks;
+	ret->reads = 0;
+	ret->writes = 0;
+	ret->block_arr = malloc(nblocks*sizeof(char *));
+	for (int i = 0; i < nblocks; i++)
+		ret->block_arr[i] = malloc(BLOCKSIZE * sizeof(char));
+
+	return ret;
+}
+
+int read_block(disk *diskptr, int blocknr, void *block_data)
+{
+	if (blocknr < 0 || blocknr > diskptr->blocks - 1)
+		return -1;
+
+	memcpy(block_data, diskptr->block_arr[blocknr], BLOCKSIZE);
+	diskptr->reads++;
+
+	return 0;
+}
+
+int write_block(disk *diskptr, int blocknr, void *block_data)
+{
+	if (blocknr < 0 || blocknr > diskptr->blocks - 1)
+		return -1;
+
+	memcpy(diskptr->block_arr[blocknr], block_data, BLOCKSIZE);
+	diskptr->writes++;
+
+	return 0;
+}
+
+int free_disk(disk *diskptr)
+{
+	if(!diskptr)
+		return 0;
+
+	for (int i = 0; i < diskptr->blocks; i++)
+		free(diskptr->block_arr[i]);
+
+	free(diskptr->block_arr);
+	free(diskptr);
+
+	return 0;
+}
--- CodeSubmission/sfs.c
+++ CodeSubmission/sfs.c
@@ -1,2 +1,91 @@
 #include "disk.h"
 #include "sfs.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+disk *g_diskptr = NULL;
+super_block *g_sb = NULL;
+
+int ceili(int numerator, int denominator)
+{
+	return (numerator / denominator + (numerator % denominator != 0));
+}
+
+int format(disk *diskptr)
+{
+	/* Calculating stuff */
+	int i, N, M, I, IB, R, DBB, DB;
+
+	N   = diskptr->blocks;
+	M   = N - 1;
+	I   = 0.1 * M;
+	IB  = ceili(I * 128, 8 * BLOCKSIZE);
+	R   = M - I - IB;
+	DBB = ceili(R, 8 * BLOCKSIZE);
+	DB  = R - DBB;
+
+	/* Creating superblock */
+	super_block sb = {
+		.magic_number           = 12345,
+		.blocks                 = M,
+		.inode_blocks           = I,
+		.inodes                 = I * 128,
+		.inode_bitmap_block_idx = 1,
+		.data_block_bitmap_idx  = 1 + IB,
+		.inode_block_idx        = 1 + IB + DBB,
+		.data_block_idx         = 1 + IB + DBB + I,
+		.data_blocks            = DB
+	};
+
+	/* Writing the superblock to disk */
+	if (write_block(diskptr, 0, &sb))
+		return -1;
+
+	/* Creating a dummy buffer to fill blocks */
+	void *buf = malloc(BLOCKSIZE * sizeof(char));
+	memset(buf, 0, BLOCKSIZE * sizeof(char));
+
+	/* Writing inode bitmap to disk */
+	for (i = sb.inode_bitmap_block_idx; i < sb.data_block_bitmap_idx; i++)
+		if (write_block(diskptr, i, buf))
+			return -1;
+
+	/* Writing datablock bitmap to disk */
+	for (i = sb.data_block_bitmap_idx; i < sb.inode_block_idx; i++)
+		if (write_block(diskptr, i, buf))
+			return -1;
+
+	/* Create a dummy inode array to fill blocks */
+	inode dum[128];
+	for (i = 0; i < 128; i++)
+		dum[i].valid = 0;
+
+	/* Writing empty inodes to disk */
+	for (i = sb.inode_block_idx; i < sb.data_block_idx; i++)
+		if (write_block(diskptr, i, (void *)dum))
+			return -1;
+
+	return 0;
+}
+
+int mount(disk *diskptr)
+{
+	if (!diskptr)
+		return -1;
+
+	/* Trying to read superblock into a temporary variable */
+	void *buf = malloc(BLOCKSIZE * sizeof(char));
+	super_block *temp = malloc(sizeof(super_block));
+	read_block(diskptr, 0, buf);
+	memcpy(temp, buf, sizeof(super_block));
+
+	if (temp->magic_number != MAGIC)
+		return -1;
+
+	/* Successful, update global state */
+	g_diskptr = diskptr;
+	g_sb = temp;
+
+	return 0;
+}
